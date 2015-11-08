8c2a310cb8f52491e3b53e5b6a9748bcb17702d4
fvandaalen <florianvandaalen@home.nl>
2015-11-08 10:06:30 +0100

    chisquared print

--- src/main/java/internship/Weka/WekaMain.java
+++ src/main/java/internship/Weka/WekaMain.java
@@ -1866,7 +1866,7 @@ public class WekaMain implements Serializable {
                 double[] chiValues = chi.ChiValues(d.data);
                 writer.write("Ensemble:" + d.start + "-" + d.end);
                 for(int i = 0; i<pValues.length; i++){
-                    writer.write(d.data.attribute(i+1).name() + " pvalue: " + pValues[i] + " chiValue: " + chiValues[i]);
+                    writer.write(d.data.attribute(i+1).name() + " pvalue: " + pValues[i] + " chiValue: " + chiValues[i] + " degrees of freedom: " + (d.data.numAttributes()-2));
 
                 }
             }
